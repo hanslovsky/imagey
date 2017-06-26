96b337ec79a7aeee51aa0e59ec9a215a60d73ae3
Philipp Hanslovsky <hanslovskyp@janelia.hhmi.org>
2017-06-26 14:55:36 -0400

    Add imagey-starter.sh
    Place imagey-starter.sh and imagey.py inside Fiji.app directory and execute imagey-starter.sh to run imagey

---
+++ imagey-starter.sh
@@ -0,0 +1,90 @@
+#!/bin/bash
+# should we assume that bash is there? does sh understand basic globbing?
+# PYJNIUS_JAR=$HOME/imglyb-jars/pyjnius.jar JAVA_HOME=/usr/lib/jvm/default USE_SYSTEM_PYTHON=1 ./imagey-starter.sh
+
+# set -e
+
+PYTHON=python
+USER_JAVA_HOME=${JAVA_HOME}
+
+export FIJI_APP_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+
+if [ -z "${USE_SYSTEM_PYTHON}" ]; then
+    CONDA_HOME=${CONDA_HOME:-$FIJI_APP_DIR/conda}
+    FIJI_CONDA_ENVIRONMENT=${FIJI_CONDA_ENVIRONMENT:-fiji}
+    unset PYTHONPATH
+
+    if [ ! -d "${CONDA_HOME}" ]; then
+        # adjust name but good for now
+        case "$(uname -s)" in
+
+            Linux)
+                INSTALLER_NAME=Miniconda3-latest-Linux-x86_64.sh
+                ;;
+            Darwin)
+                INSTALLER_NAME=Miniconda3-latest-MacOSX-x86_64.sh
+                ;;
+            *)
+                echo "Do not understand OS: " $(uname -s) >&2
+                exit 1
+        esac
+        CONDA_INSTALLER="${FIJI_APP_DIR}/${INSTALLER_NAME}"
+        wget \
+            https://repo.continuum.io/miniconda/${INSTALLER_NAME} \
+            -O ${CONDA_INSTALLER}
+        sh "${CONDA_INSTALLER}" -b -p $CONDA_HOME
+        rm "${CONDA_INSTALLER}"
+    fi
+
+    export PATH="${CONDA_HOME}/bin:$PATH"
+
+    CONDA_CMD="${CONDA_HOME}/bin/conda"
+    ACTIVATE="${CONDA_HOME}/bin/activate"
+
+
+    # how to ignore ~/.conda?
+    # CONDA_ENVS_PATH='' /home/phil/local/Fiji.app/conda/bin/conda config --show
+    # does not work
+    CONDA_ENVS_INFO=$(${CONDA_CMD} info --envs)
+    INFO_GREP_OPTS="-E '^\${FIJI_CONDA_ENVIRONMENT} +[*/]'"
+    ENV_EXIST_STRING=$( echo "$CONDA_ENVS_INFO" | grep -E "^${FIJI_CONDA_ENVIRONMENT} +[*/]" )
+    if [ -z "${ENV_EXIST_STRING}" ]; then
+        # probably need to specify version for imglib2-imglyb
+        CREATE_CMD="${CONDA_CMD} create \
+              -y \
+              -n ${FIJI_CONDA_ENVIRONMENT} \
+              -c hanslovsky \
+              python=3.6 \
+              imglib2-imglyb \
+              pyqt \
+              qtconsole \
+              scikit-image \
+              matplotlib"
+        ${CREATE_CMD}
+    fi
+
+    # recommended by conda installation instructions to source activate first:
+    # https://conda.io/docs/help/silent.html#linux-and-os-x
+    source "${ACTIVATE}" "${FIJI_CONDA_ENVIRONMENT}"
+    export JAVA_HOME="${USER_JAVA_HOME:-$JAVA_HOME}"
+
+fi
+
+
+JARS_DIR="${FIJI_APP_DIR}/jars"
+PLUGINS_DIR="${FIJI_APP_DIR}/plugins"
+BIO_FORMATS_DIR="${JARS_DIR}/bio-formats"
+
+JARS_FMT="${JARS_DIR}/*.jar"
+PLUGINS_FMT="${PLUGINS_DIR}/*.jar"
+BIO_FORMATS_FMT="${BIO_FORMATS_DIR}/*.jar"
+
+export CLASSPATH=`ls -1 ${JARS_FMT} ${PLUGINS_FMT} ${BIO_FORMATS_FMT} | tr '\n' ':'`
+# echo $CLASSPATH | tr ':' '\n'
+export IMGLYB_JAR=`ls -1 ${JARS_DIR}/imglib2-imglyb*.jar | head -n1`
+# echo $IMGLYB_JAR
+
+
+$PYTHON "${FIJI_APP_DIR}/imagey.py" "$@"
+
