a651ca17798d2e009c21ed8e18a33106fa14f567
Philipp Hanslovsky <hanslovskyp@janelia.hhmi.org>
2018-06-15 17:27:50 -0400

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+from distutils.command.build_py import build_py
+
+setup(
+    name='imagey',
+    version='0.0.1',
+    author='Philipp Hanslovsky',
+    author_email='hanslovskyp@janelia.hhmi.org',
+    description='imagey launcher',
+    url='https://github.com/hanslovsky/imagey',
+    packages=['imagey'],
+    entry_points={
+        'console_scripts': [
+            'imagey=imagey.imagey:main'
+        ]
+    },
+)
